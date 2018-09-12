5d8853b6cb266ec1a992976d35ee1e28a68db261
Yaroslav Yudinskikh <35652747+SimonWT@users.noreply.github.com>
2018-09-12 22:39:38 +0300

    Comments to exercise 2

--- week4/ex2.c
+++ week4/ex2.c
@@ -1,16 +1,48 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <unistd.h>
-#include <stdlib.h>
 
 int main(){
 
-   for (int i = 0; i< 5; i++){
-     fork();
-   }
+     for (int i = 0; i< 3; i++){
+       fork();
+     }
 
-  // printf("Privetik ot Rinata\n");
-   sleep(5000);
+   delay(5000);
    return 0;
 }
 
+/*
+3 times call for();
+
+init───init───bash───bash───sudo───bash───bash─┬─ex2.out─┬─ex2.out─┬─ex2.out───ex2.out
+                                               │         │         └─ex2.out
+                                               │         ├─ex2.out───ex2.out
+                                               │         └─ex2.out
+                                               ├─2*[nano]
+                                               └─pstree
+
+5 times call for();
+
+init───init───bash───bash───sudo───bash───bash─┬─ex2.out─┬─ex2.out─┬─ex2.out─┬─ex2.out─┬─ex2.out
+                                              │         │         │         │         └─ex2.out
+                                              │         │         │         ├─ex2.out───ex2.out
+                                              │         │         │         └─ex2.out
+                                              │         │         ├─ex2.out─┬─ex2.out───ex2.out
+                                              │         │         │         └─ex2.out
+                                              │         │         ├─ex2.out───ex2.out
+                                              │         │         └─ex2.out
+                                              │         ├─ex2.out─┬─ex2.out─┬─ex2.out───ex2.out
+                                              │         │         │         └─ex2.out
+                                              │         │         ├─ex2.out───ex2.out
+                                              │         │         └─ex2.out
+                                              │         ├─ex2.out─┬─ex2.out───ex2.out
+                                              │         │         └─ex2.out
+                                              │         ├─ex2.out───ex2.out
+                                              │         └─ex2.out
+                                              ├─2*[nano]
+                                              └─pstree
+
+fork create copy of parent job i times. So in different i, we have more different number of childs.                                             
+
+*/
