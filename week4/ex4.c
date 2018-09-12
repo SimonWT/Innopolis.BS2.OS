a417a7a1e40434b521ef2e5316be7c52d59eba00
root <root@DESKTOP-2JVMVJS.localdomain>
2018-09-13 00:45:38 +0300

    ex4

--- week4/ex4.c
+++ week4/ex4.c
@@ -2,16 +2,47 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-
 int main(){
    char command[100];
+   char cmd[10];
+   char argument[100];
+
    while(1){
 
      printf("\nYour command: \n>");
      fgets(command, 100, stdin);
-     system(command);
 
-          }
+     int dif;
+
+     for(int i=0; i< 100; i++){
+      //printf("%d - '%c' \n", i, command[i]);
+       if(command[i]==' '){
+         dif = i+1;
+         for(int j=0; j<i; j++){
+           cmd[j] = command[j];
+           if(j==i-1) cmd[j+1] = '\0';
+         }
+        break;
+       }
+     }
+
+     for(int k = 0; k < (100 - dif); k++){
+       argument[k] = command[k+dif];
+     }
+
+     //printf("%s\n%s\n", cmd, argument);
+
+
+     char* argv[] = {cmd, argument, NULL};
+
+
+    //system(command);
+     fork();
+
+     execvp(argv[0], argv);
+
+   }
+
 
    return 0;
 }
