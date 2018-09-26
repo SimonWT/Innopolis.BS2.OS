6c27d72bcaaf501ae9f9e57b615482f82b5ce72a
root <root@DESKTOP-2JVMVJS.localdomain>
2018-09-27 00:52:38 +0300

    ex6

--- week6/ex6.c
+++ week6/ex6.c
@@ -2,32 +2,40 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <signal.h>
+#include <sys/wait.h>
 #define TRUE 1
 
 int main(){
   int pipefd[2];
   pipe(pipefd);
+  int status;
 
-  int fork_status1;
-  int fork_status2;
+  pid_t fork_status1;
+  pid_t fork_status2;
 
   fork_status1 = fork();
-  if(fork_status1 == 0){
+
+  if(fork_status1){
     fork_status2 = fork();
-    if(fork_status2 == 0){
-      int pid= getpid();
-      write(pipefd[1], pid, 500);
-      printf("[Child 2]: pid = %d\n..1.Send pid to 1st child..\n", pid);
+
+    if(fork_status2){
+      printf("..Send 2st child pid to 1st child..\n", (int) fork_status2);
+      write(pipefd[1], &fork_status2, sizeof(fork_status2));
+      printf("..Wait for state changes in second child\n");
+      waitpid(fork_status2, &status, 0);
+      printf("Got status form 2d child: %d\n",status );
 
     }else{
-      int child_pid, status;
-      read(pipefd[0], child_pid, 500);
-      printf("[Child 1]: child2 pid = %d\n", child_pid);
-      waitpid(child_pid, status, 0);
 
     }
   }else{
+    sleep(4);
+    int child_pid;
+    read(pipefd[0], &child_pid, sizeof(child_pid));
+    printf("[Child 1]: child2 pid = %d\n", child_pid);
 
+    printf("..Killing child 2..\n");
+    kill(child_pid, SIGKILL);
   }
 
 
