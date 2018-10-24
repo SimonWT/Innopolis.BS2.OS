9da8ed3f68062bedefc1b21f93ac0609a99a3995
root <root@DESKTOP-2JVMVJS.localdomain>
2018-10-24 16:37:27 +0300

    week10/ex4

--- week10/ex4.c
+++ week10/ex4.c
@@ -4,73 +4,63 @@
 #include <sys/stat.h>
 #include <stdbool.h>
 
-typedef struct File{
-   ino_t inode;
-   bool flag;
-   int nlink;
-   char* name;
-}File;
+// typedef struct File{
+//    ino_t inode;
+//    bool flag;
+//    int nlink;
+//    char* name;
+// }File;
 
 int main(){
   struct stat *buff = malloc(sizeof(struct stat));
   int count = 0;
 
-  DIR* dirp = opendir("./tmp");
-  
+  DIR* dirp = opendir("tmp");
+
   struct dirent* dp;
 
   while ((dp = readdir(dirp)) != NULL) {
-       stat(dp->d_name, buff );
+         printf("%s\n", dp->d_name);
          count++;
     }
 
-  File files[count];
+  dirp = opendir("tmp");
+
+  struct dirent dps[count];
 
   int i = 0;
   while ((dp = readdir(dirp)) != NULL) {
-       stat(dp->d_name, buff);
-         files[i].inode = &buff->st_ino;
-         files[i].flag = false;
-         files[i].name = dp->d_name;
-         files[i].nlink = &buff->st_nlink;
+       dps[i] = *dp;
+       printf("%s\n", dps[i].d_name);
+       i++;
     }
 
 
-
-    for(int j=0; j<count; j++){
-      if(files[j].nlink > 1){
-        files[j].flag = true;
-        printf("\n%s (%lu) : ", files[j].name , files[j].inode);
+    ino_t inode;
+    for(int j=0; j < count; j++){
+      char *p1 = malloc(100);
+      strcat(p1, "./tmp/");
+      strcat(p1, dps[j].d_name );
+      int info = stat( p1, buff);
+      printf("%d Check: %s, %d\n", info, dps[j].d_name, buff->st_nlink);
+      if( buff -> st_nlink > 1 ){
+        inode = buff->st_ino;
+        printf("\n%s (%llu) : ", dps[j].d_name , inode);
+        dps[j].d_ino = 0;
          for(int k=0; k<count; k++){
-           if(!files[k].flag && files[j].inode == files[k].inode ){
-             printf("%s (%lu), ", files[k].name , files[k].inode);
-             files[k].flag = true;
+           if(dps[k].d_ino!=0){
+             char *p = malloc(100);
+             strcat(p, "./tmp/");
+             strcat(p, dps[k].d_name );
+             stat( p, buff);
+             if(buff->st_ino == inode ){
+               printf("%s (%llu), ", dps[k].d_name , buff->st_ino);
+               dps[k].d_ino = 0;
+            }
            }
          }
        }
      }
-
-
-  //int j = 0;
-  // while ((dp = readdir(dirp)) != NULL) {
-  //      stat(dp->d_name, buff );
-  //      if(buff->st_nlink > 1){
-  //        files[j]->flag = true;
-  //        files[j]->inode = buff->st_ino;
-  //        printf("%s (%lu) : ", dp->d_name, buff->st_ino);
-  //        j++;
-  //
-  //        //Search
-  //        struct stat hbuff;
-  //        while((hdp = readdir(dirp)) != NULL ){
-  //           stat(hdp->d_name, hbuff);
-  //           if(hbuff -> st_ino == buff -> st_ino){
-  //
-  //           }
-  //        }
-  //
-  //      }
-  //   }
 
   (void)closedir(dirp);
 	return 0;
