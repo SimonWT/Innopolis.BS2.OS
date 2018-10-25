53e538e4b3e71e26f6b0e9d9ea2825e08e6e9254
root <root@DESKTOP-2JVMVJS.localdomain>
2018-10-25 10:45:28 +0300

    week10/ex4 comments and beautify

--- week10/ex4.c
+++ week10/ex4.c
@@ -4,65 +4,62 @@
 #include <sys/stat.h>
 #include <stdbool.h>
 
-// typedef struct File{
-//    ino_t inode;
-//    bool flag;
-//    int nlink;
-//    char* name;
-// }File;
-
 int main(){
-  struct stat *buff = malloc(sizeof(struct stat));
-  int count = 0;
+        struct stat *buff = malloc(sizeof(struct stat));
+        int count = 0;
 
-  DIR* dirp = opendir("tmp");
+        DIR* dirp = opendir("tmp");
+        struct dirent* dp;
 
-  struct dirent* dp;
+        //Count how many files in directory
+        while ((dp = readdir(dirp)) != NULL) {
+                count++;
+        }
 
-  while ((dp = readdir(dirp)) != NULL) {
-         printf("%s\n", dp->d_name);
-         count++;
-    }
+        dirp = opendir("tmp");
 
-  dirp = opendir("tmp");
+        //Store files in directories in array
+        struct dirent dps[count];
 
-  struct dirent dps[count];
-
-  int i = 0;
-  while ((dp = readdir(dirp)) != NULL) {
-       dps[i] = *dp;
-       printf("%s\n", dps[i].d_name);
-       i++;
-    }
+        //File arrays of that files
+        int i = 0;
+        while ((dp = readdir(dirp)) != NULL) {
+                dps[i] = *dp;
+                i++;
+        }
 
 
-    ino_t inode;
-    for(int j=0; j < count; j++){
-      char *p1 = malloc(100);
-      strcat(p1, "./tmp/");
-      strcat(p1, dps[j].d_name );
-      int info = stat( p1, buff);
-      printf("%d Check: %s, %d\n", info, dps[j].d_name, buff->st_nlink);
-      if( buff -> st_nlink > 1 ){
-        inode = buff->st_ino;
-        printf("\n%s (%llu) : ", dps[j].d_name , inode);
-        dps[j].d_ino = 0;
-         for(int k=0; k<count; k++){
-           if(dps[k].d_ino!=0){
-             char *p = malloc(100);
-             strcat(p, "./tmp/");
-             strcat(p, dps[k].d_name );
-             stat( p, buff);
-             if(buff->st_ino == inode ){
-               printf("%s (%llu), ", dps[k].d_name , buff->st_ino);
-               dps[k].d_ino = 0;
-            }
-           }
-         }
-       }
-     }
+        ino_t inode;
+        for(int j=0; j < count; j++) {
+                char *path = malloc(100); //Create a path to the files
+                strcat(path, "./tmp/");
+                strcat(path, dps[j].d_name );
+                stat(path, buff);
 
-  (void)closedir(dirp);
-	return 0;
+
+                //Find files where number of links more than one and didn't already visited
+                if( buff->st_nlink > 1 && dps[j].d_ino != 0) {
+                        inode = buff->st_ino; //Store i-node number of that file
+                        printf("\n%s (i-node #: %llu ) : ", dps[j].d_name , inode);
+                        dps[j].d_ino = 0; //Mark it bs we had seen that
+                        struct stat *buff_supp = malloc(sizeof(struct stat));
+                        for(int k=0; k<count; k++) {
+                                //Find all files that point to the file
+                                if(dps[k].d_ino!=0) { // Check visited it or not
+                                        char *path_supp = malloc(100); //Create a path to the files
+                                        strcat(path_supp, "./tmp/");
+                                        strcat(path_supp, dps[k].d_name );
+                                        stat(path_supp, buff_supp); //Get stat of that
+                                        if(buff_supp->st_ino == inode ) { //Check is it file that point to our
+                                                printf("%s (i-node #: %llu ) ", dps[k].d_name, buff_supp->st_ino);
+                                                dps[k].d_ino = 0; //Mark it visited
+                                        }
+                                }
+                        }
+                }
+        }
+
+        (void)closedir(dirp);
+        return 0;
 }
 
